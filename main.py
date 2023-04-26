b8627dd7a5213b7de0c28136fd2aefd64f5e1af7
MikeDestroyer <Treil911@gmail.com>
2023-04-26 17:21:23 +0300

    removed comments

--- main.py
+++ main.py
@@ -5,38 +5,26 @@ import random
 import config
 from ascii import logo, stages
 
-# def game_menu(state):
-#     clear()
-#     if state == 0:
-#         print(logo)
-#         print("Welcome to CUM ZONE")
-#     elif state == 1:
-#         print("Win!")
-#     elif state == 2:
-#         print("Game over..")
-#     if input("Press Enter to start new game ") == "" :
-#         gameplay()
-#     else:
-#         print("Game closed")
-#         exit(0)
-
 def gameplay():
     print(logo)
-    print("Welcome to CUM ZONE")
-
     stats = config.stats()
+
     while(stats.health != 0):
         correct = False
         key = input("Enter word: ")
+
         for index, letter in enumerate(stats.wordline):
             if key == letter:
                 correct = True
                 stats.wordboard[index] = stats.wordline[index]
+
         if correct == False : stats.health -= 1
         word_display(stats)
+
         if stats.health == 0:
             print("You lose. The ungessed word was", "".join(stats.wordline).upper())
             exit(0)
+
         if "_" not in stats.wordboard:
             print("You win")
             exit(0)
